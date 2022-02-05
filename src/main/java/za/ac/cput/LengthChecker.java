5bde14a0ddf9075544783b542031234cd83add8e
ChanteLeeDavids <220246688@mycput.ac.za>
2022-02-05 23:17:18 +0200

    LengthChecker project template

---
+++ src/main/java/za/ac/cput/LengthChecker.java
@@ -0,0 +1,12 @@
+/*
+    LengthChecker.java
+    This application checks the length of a string
+    Chante Lee Davids [220246688]
+    5/02/2022
+ */
+
+package za.ac.cput;
+
+public class LengthChecker {
+
+}
---
+++ src/test/java/za/ac/cput/LengthCheckerTest.java
@@ -0,0 +1,17 @@
+/*
+    LengthCheckerTest.java
+    Tests whether LengthChecker.java checks the length of a string
+    Chante Lee Davids [220246688]
+    5/02/2022
+ */
+
+package za.ac.cput;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LengthCheckerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+}
